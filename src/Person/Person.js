ea8fe23c3984edd85e59e70d27485cd2f54b4413
He Zhang <rbczhang90@gmail.com>
2019-02-04 15:35:50 -0800

    Add css module files for components

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,10 +1,10 @@
 import React from 'react';
-
+import styles from './Person.module.css'
 //ES6 best practice
 const person = (props) => {
     // JSX syntax
     return ( 
-        <div>
+        <div className={styles.Person}>
             <p onClick={props.click}> I'm {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name}></input>
